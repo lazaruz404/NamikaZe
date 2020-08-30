d60f54af7756335926a7f49889f68cf34bd3cad6
abu38 <69963310+abu38@users.noreply.github.com>
2020-08-30 22:45:37 +0800

    Add files via upload

--- userbot/modules/snips.py
+++ userbot/modules/snips.py
@@ -70,14 +70,14 @@ async def on_snip_save(event):
     elif event.reply_to_msg_id and not string:
         rep_msg = await event.get_reply_message()
         string = rep_msg.text
-    success = "`Snip {} successfully. Use` **${}** `anywhere to get it`"
+    success = "`Snip {} successfully. Use` **.{}** `anywhere to get it`"
     if add_snip(keyword, string, msg_id) is False:
         await event.edit(success.format('updated', keyword))
     else:
         await event.edit(success.format('saved', keyword))
 
 
-@register(outgoing=True, pattern="^.snips$")
+@register(outgoing=True, pattern="^.snips.")
 async def on_snip_list(event):
     """ For .snips command, lists snips saved by you. """
     try:
@@ -91,9 +91,9 @@ async def on_snip_list(event):
     for a_snip in all_snips:
         if message == "`No snips available right now.`":
             message = "Available snips:\n"
-            message += f"`${a_snip.snip}`\n"
+            message += f"`.{a_snip.snip}`\n"
         else:
-            message += f"`${a_snip.snip}`\n"
+            message += f"`.{a_snip.snip}`\n"
 
     await event.edit(message)
 
@@ -116,7 +116,7 @@ async def on_snip_delete(event):
 CMD_HELP.update({
     "snips":
     "\
-$<snip_name>\
+.<snip_name>\
 \nUsage: Gets the specified snip, anywhere.\
 \n\n.snip <name> <data> or reply to a message with .snip <name>\
 \nUsage: Saves the message as a snip (global note) with the name. (Works with pics, docs, and stickers too!)\
