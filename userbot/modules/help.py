36bfb9178a80f2b4333e73e4c6cb1385decf7e7c
abu38 <69963310+abu38@users.noreply.github.com>
2020-08-31 16:11:49 +0800

    Add files via upload

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -17,7 +17,7 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("Please specify a valid module name.")
+            await event.edit("Modul Salah Goblokk.")
     else:
         string = ""
         for i in CMD_HELP:
