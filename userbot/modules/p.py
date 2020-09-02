84d4ce059ab180fc2ac74d916a5963c3ecdaf116
abu38 <69963310+abu38@users.noreply.github.com>
2020-09-02 11:09:36 +0800

    Add files via upload

--- userbot/modules/p.py
+++ userbot/modules/p.py
@@ -12,11 +12,11 @@ async def typewriter(typew):
 # Create by myself @rizkynfs
 
 
-@register(outgoing=True, pattern='^P(?: |$)(.*)')
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("Biasakanlah Mengucapkan Salam")
+    await typew.edit("Jawab Salam Boss Astaga")
     sleep(1)
-    await typew.edit("Assalamualaikum")
+    await typew.edit("Waalaikumsalam")
 # Create by myself @rizkynfs
